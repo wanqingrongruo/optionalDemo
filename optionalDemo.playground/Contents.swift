3b7b8f4db7112701277a3e0b98c53075875ef4a4
婉卿容若 <devzheng@qq.com>
2016-12-12 09:07:36 +0800

    g

--- optionalDemo.playground/Contents.swift
+++ optionalDemo.playground/Contents.swift
@@ -3,4 +3,66 @@
 import UIKit
 
 var str = "Hello, playground"
-// sentinal value 哨兵值
+
+extension Array where Element: Equatable{
+    func find(_ element: Element) -> Optional<Index>{
+        var index = startIndex
+        
+        while index != endIndex {
+            if self[index] == element {
+                return .some(index)
+            }
+            
+            formIndex(after: &index)
+        }
+        
+        return .none
+    }
+    
+    
+    func find02(_ element: Element) -> Index?{
+        var index = startIndex
+        
+        while index != endIndex {
+            if self[index] == element {
+                return index
+            }
+            
+            formIndex(after: &index)
+        }
+        
+        return nil
+    }
+    
+}
+
+
+var numbers = [1,2,3]
+let index = numbers.find(2)
+
+print(type(of: index))
+
+
+switch index {
+case .some(let index):
+    numbers.remove(at: index)
+case .none:
+    print("Not exits")
+}
+
+
+var numbers02 = [3,4,5]
+let index02 = numbers02.find02(5)
+switch index02 {
+case let index?:
+    numbers02.remove(at: index)
+case nil:
+    print("Not exits")
+
+}
+
+// force unwrapping => value!
+// optinal binding  => if let value = value
+// optional chaining => value?.
+// nil coalescing => value ?? default
+
